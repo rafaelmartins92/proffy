cd8f25b004463c1e87d068fe106cd48667dbb215
Rafael Martins <rafael.martins92@outlook.com>
2020-10-05 22:45:56 -0300

    chore: create database

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'})
+  return response.json({ message: 'Hello World'});
 })
 
 app.listen(3333)
